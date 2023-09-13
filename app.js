b519db704e8025382475fbd121b0ecafa79af7b1
Meghan Grousset <megh.g@hotmail.fr>
2023-09-13 15:24:37 +0200

    add : création schema

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
 const app = express();
 
-app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
+app.use(express.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/LooseWaves',
+
+   {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+   })
+   .then(() => console.log('Connexion à MongoDB réussie !'))
+   .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+   res.setHeader('Access-Control-Allow-Origin', '*');
+   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+   next();
+});
+
+
+app.post('/api/stuff', (req, res, next) => {
+   console.log(req.body);
+   res.status(201).json({
+      message: 'Objet créé !'
+   });
 });
 
 module.exports = app;
---
+++ models/surfspotModel.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+const surfspotSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    destination: {
+        type: String,
+        required: true,
+    },
+    difficulty: {
+        type: Number,
+        required: true,
+    },
+    surfBreak: {
+        type: String,
+        required: true,
+    },
+    photo: {
+        type: Image,
+        required: false,
+    },
+    seasonBegin: {
+        type: Date,
+        required: true,
+    },
+    seasonEnd: {
+        type: Date,
+        required: true,
+    },
+});
+
+const surfspotModel = mongoose.model('surfspots', surfspotSchema);
+module.exports = surfspotModel;
