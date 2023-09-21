fd97f37f3b0afc4883462b337975c65428334736
FlorianLUSSON <florianlusson@gmail.com>
2023-09-21 11:36:29 +0200

    Lien base de donnée Atlas effectué

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const routeSurfSpots = require('./routes/surfspots');
 const routeUsers = require('./routes/users')
 
-mongoose.connect('mongodb://127.0.0.1:27017/LooseWaves',
+mongoose.connect('mongodb+srv://admin:loosewaves@loosewaves.2hmpcb5.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp',
 
    {
       useNewUrlParser: true,
